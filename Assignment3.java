af35b0d91b399eb5c163c542b089e07ca2b31ffa
Aurélien <Aurelien@dyn221216.shef.ac.uk>
2020-02-01 15:11:18 +0100

    Hello

--- Assignment3.java
+++ Assignment3.java
@@ -1,5 +1,7 @@
 import ShefRobot.*;
 
 public class Assignment3 {
-	
+	public static void main (String[] args) {
+		System.out.print("hello darkness my old friend, i've come to talk with you, again");
+	}
 }
