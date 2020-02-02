acea096538984f62345e038e34100e954ea3f543
Zbyszek Lisak <zbigniew.lisak68@gmail.com>
2020-02-02 21:01:11 +0000

    Hop

--- Assignment3.java
+++ Assignment3.java
@@ -2,9 +2,15 @@ import ShefRobot.*;
 
 public class Assignment3 {
 
+    private Robot myRobot;
+
+    private void init(){
+        this.myRobot = new Robot();
+    }
     public static void main(String[] args) {
         
-        init();
+        Assignment3 main = new Assignment3();
+        main.init();
     
     }
 
