c3fb633941e7696912c2ddab9c62c7336c64abda
Zbyszek Lisak <zbigniew.lisak68@gmail.com>
2020-02-02 21:21:07 +0000

    Init Ready

--- Assignment3.java
+++ Assignment3.java
@@ -1,17 +1,45 @@
 import ShefRobot.*;
 
 public class Assignment3 {
-	
-    private Robot myRobot;
 
-    private void init(){
+    private Robot myRobot;
+    private LargeMotor left;
+    private LargeMotor right;
+    private MediumMotor trap;
+    private ColorSensor colorSensor;
+    private TouchSensor touchSensor;
+    private boolean ballDropped;
+
+    public Assignment3() {
+
+        this.myRobot = null;
+        this.right = null;
+        this.left = null;
+        this.trap = null;
+        this.colorSensor = null;
+        this.touchSensor = null;
+
+    }
+
+    public Assignment3(String robotId, Motor.Port right, Motor.Port left, Motor.Port trap, Sensor.Port touch,
+            Sensor.Port color) {
+
+        this.myRobot = new Robot(robotId);
+        this.right = myRobot.getlargeMotor(right);
+        this.left = myRobot.getLargeMotor(left);
+        this.trap = myRobot.getMediumMotor(trap);
+        this.colorSensor = myRobot.getCoulorSensor(color);
+        this.touchSensor = myRobot.getTouchSensor(touch);
+    }
+
+    private void init() {
         this.myRobot = new Robot();
     }
+
     public static void main(String[] args) {
-        
+
         Assignment3 main = new Assignment3();
         main.init();
 
-    
     }
 }
